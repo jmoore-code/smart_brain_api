08abca2e9f9ddfcacfad7270255064f60d3a3301
Unnamed-one <55922515+Unnamed-one@users.noreply.github.com>
2020-03-31 13:19:41 -0600

    format files

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,32 @@
-const Clarifai = require('clarifai')
+const Clarifai = require("clarifai");
 
 // You must add your own API key here from Clarifai
 const app = new Clarifai.App({
-    apiKey: process.env.API_CLARIFAI
-  });
+  apiKey: process.env.API_CLARIFAI
+});
 
-  const handleApiCall = (req, res) => {
-    app.models
+const handleApiCall = (req, res) => {
+  app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-        res.json(data)
+      res.json(data);
     })
-    .catch(err => res.status(400).json("unable to work with api"))
-  }
+    .catch(err => res.status(400).json("unable to work with api"));
+};
 
 const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
     .then(entries => {
-        res.json(entries[0])
+      res.json(entries[0]);
     })
-    .catch(err => res.status(400).json('unable to get entries'))
-}
+    .catch(err => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
-    handleImage,
-    handleApiCall
-}
+  handleImage,
+  handleApiCall
+};
